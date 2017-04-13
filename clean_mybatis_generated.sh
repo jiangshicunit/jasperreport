21f5d7cb0cce9c8b93931b627c2cc7b6724920dd
管鹏波 <guanpengbo@haomo-studio.com>
2017-04-13 20:39:58 +0800

    update

--- clean_mybatis_generated.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf dao/src/main/java/com/haomostudio/SeekTruthBackend/*
-rm -rf domain/src/main/java/com/haomostudio/SeekTruthBackend/*
-rm -rf web/src/main/resources/mybatis/com/haomostudio/SeekTruthBackend/*
--- clean_mybatis_generated_springmvc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf dao/src/main/java/com/haomostudio/jrs/dao/*
-rm -rf domain/src/main/java/com/haomostudio/jrs/domain/*
-rm -rf web/src/main/resources/mybatis/com/haomostudio/jrs/crm/*
