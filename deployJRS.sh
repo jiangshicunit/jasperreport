b9ea8c8bf21b8c9ef331e4c6cedee7aaa4ee6c9c
= <=>
2017-12-01 16:26:09 +0800

    修改境外汇款申请说

--- deployJRS.sh
+++ deployJRS.sh
@@ -1,1 +1,2 @@
 rsync -avz ./web/target/jrs.war root@39.106.104.195:/root/data/apache-tomcat-8.0.47/webapps/jrs.war
+#rsync -avz ./web/target/jrs.war e-troin@192.168.0.200:/data/Tomcat8/webapps/jrs.war
