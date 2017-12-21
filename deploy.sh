a1a18df8c765757a386c2f848e583638c111569c
= <=>
2017-12-21 14:24:02 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz web/target/jrs.war root@39.106.104.195:/root/data/apache-tomcat-8.0.47/webapps/jrs.war
+rsync -avz web/target/jrs.war root@39.106.104.195:/var/lib/tomcat8/webapps/jrs.war
